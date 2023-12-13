c004dba4ea8c4cb1bd4faa651f8023a2eaa23198
FacuPorra <facu17a@gmail.com>
2023-12-13 18:38:53 -0300

    first commit

---
+++ Tp1.js
@@ -0,0 +1,26 @@
+//ejercicio 1
+let nombre="Facundo"
+let apellido= "Porra"
+let edad= "28"
+console.log("Hola,",nombre,apellido,".Tienes",edad, "años")
+//ejercicio 2
+let persona = {nombre:'Facundo',apellido:'Porra',edad: "28"}
+JSON.stringify(persona)
+//ejercicio 3
+let num1=2 ; num2=4 ; num3=6 ; num4=8
+console.log(num1+num2)
+let num5=6
+console.log(num4-num3)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ej3.js
@@ -0,0 +1,16 @@
+//ejercicio 3
+let num1=11 ; num2=11 ; num3=6 ; num4=8
+console.log(num1+num2)
+let num5=22
+console.log(num4-num3)
+let num6=2
+console.log(num5*num6)
+let resultadoFinal=44
+let esPar = (resultadoFinal) => resultadoFinal%2===0 ; 
+let esImpar = (resultadoFinal) => resultadoFinal%2===1 ;
+console.log (
+    'El {resultadoFinal} PAR, {esPar(resultadoFinal) }' ) ;
+ console.log (
+     'El {resultadoFinal} IMPAR, {esImpar(resultadoFinal) }') ;  
+
+
